bb0794cc404712e91ea042c9ca50afc6c71f67e3
Marco Antonio Ticona Rios <maticonar@pj-scr.poderjudicial.gov.bo>
2025-11-14 16:08:03 -0400

    ajustes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,63 +1,26 @@
-import { Body, Controller, Get, Logger, Post, Res } from '@nestjs/common';
-import type { Response } from 'express';
+import { Controller, Post, Req, Header, type RawBodyRequest } from '@nestjs/common';
+import { Request } from 'express';
 import { createHmac } from 'crypto';
-
-interface ZoomWebhookPayload {
-  plainToken: string;
-  encryptedToken?: string;
-  object?: any;
-}
-
-interface ZoomWebhookBody {
-  event: string;
-  event_ts: number;
-  payload: ZoomWebhookPayload;
-}
 
 @Controller('webhook')
 export class AppController {
-  private readonly logger = new Logger(AppController.name);
-
-  @Get()
-  test() {
-    return 'hola mundo';
-  }
-
   @Post()
-  handleWebhook(@Body() body: ZoomWebhookBody, @Res() res: Response) {
-    if (!body) {
-      this.logger.warn('Webhook recibido sin body');
-      return res.json({ message: 'Body vacío' });
-    }
-
+  @Header('Content-Type', 'application/json')
+  handleWebhook(@Req() req: RawBodyRequest<Request>) {
+    const body = JSON.parse((req.rawBody ?? '').toString());
     const zoomSecret = process.env.ZOOM_SECRET_TOKEN ?? '';
 
-    console.log('BODY RECIBIDO ===>', body);
-    console.log('ZOOM SECRET ===>', zoomSecret);
-
-    // 🔹 Validación del endpoint
     if (body.event === 'endpoint.url_validation') {
-      const plainToken = body.payload.plainToken;
-
       const encryptedToken = createHmac('sha256', zoomSecret)
-        .update(plainToken)
+        .update(body.payload.plainToken)
         .digest('hex');
 
-      console.log('Enviando respuesta de validación:', {
-        plainToken,
+      return {
+        plainToken: body.payload.plainToken,
         encryptedToken,
-      });
-
-      // 🔹 Respuesta EXACTA para Zoom
-      return res.json({
-        plainToken,
-        encryptedToken,
-      });
+      };
     }
 
-    // 🔹 Otros eventos
-    this.logger.log('Evento recibido:', body.event);
-
-    return res.json({ status: 'ok' });
+    return { status: 'ok' };
   }
 }
